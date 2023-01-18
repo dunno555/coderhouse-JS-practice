3530f31321bbccf38ec0f62eab4c03b359f485f8
dunno555 <bimbo.sidle@gmail.com>
2023-01-18 17:23:10 -0300

    refactored for loop and conditions

--- JavaScript_projects/Clase 3/app1.js
+++ JavaScript_projects/Clase 3/app1.js
@@ -1,21 +1,16 @@
 let password = prompt('Enter your password');
 
-for (let i = 3; i > 0;) {
+for (let i = 2; i >= 0; i--) {
     if (password.toLowerCase() == 'esfera') {
         alert('Access granted');
-        i = -1; // Esto me parece horrible. Pienso que debe haber alguna mejor manera para solucionarlo
-    } else {
-        i--;
-        
-        // Sospecho lo mismo acá, que debe haber alguna mejor manera de expresarlo
-
-        if (i > 0) {
-            alert(`Access denied. Please, try again. You have ${i} remaining access attempt(s)`);
-            password = prompt('Enter your password');
-        };
-
-        if (i == 0) {
-            alert(`Your account has been blocked. Please, contact an administrator to review your current access`);
-        };
+        break;
     };
+
+    if (i == 0) {
+        alert(`Your account has been blocked. Please, contact an administrator to review your current access`);
+        break;
+    };
+
+    alert(`Access denied. Please, try again. You have ${i} remaining access attempt(s)`);
+    password = prompt('Enter your password');
 };
