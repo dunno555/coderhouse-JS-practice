c9b5123f85b270fdfa06f6addcdf2fcfeecf0b65
dunno555 <bimbo.sidle@gmail.com>
2023-01-18 11:30:04 -0300

    added comments for Tutor review

--- JavaScript_projects/Clase 3/app1.js
+++ JavaScript_projects/Clase 3/app1.js
@@ -3,9 +3,11 @@ let password = prompt('Enter your password');
 for (let i = 3; i > 0;) {
     if (password.toLowerCase() == 'esfera') {
         alert('Access granted');
-        i = -1;
+        i = -1; // Esto me parece horrible. Pienso que debe haber alguna mejor manera para solucionarlo
     } else {
         i--;
+        
+        // Sospecho lo mismo acá, que debe haber alguna mejor manera de expresarlo
 
         if (i > 0) {
             alert(`Access denied. Please, try again. You have ${i} remaining access attempt(s)`);
